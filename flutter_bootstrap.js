81dc86e2420777ae49be1789f9d2981d64378728
Ilelaboye Lekan <40622920+ilelaboye@users.noreply.github.com>
2025-06-17 02:19:34 +0100

    design corrections

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "649064647"
+    serviceWorkerVersion: "2200164406"
   }
 });
